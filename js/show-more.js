b0cc7e2f087895d67e0427b605b59f21ea109e44
asifhaiat <asifhaiat674652gmail.com>
2022-11-05 19:10:34 +0600

    updated review section and show more, show less button

--- js/show-more.js
+++ js/show-more.js
@@ -1,3 +1,4 @@
+/* second review show more text
 const parentContainer = document.querySelector(".read-more-container");
 
 parentContainer.addEventListener("click", (event) => {
@@ -7,37 +8,45 @@ parentContainer.addEventListener("click", (event) => {
   const currentText = event.target.parentNode.querySelector(".read-more-text");
   currentText.classList.toggle("read-more-text--show");
   current.textContent = current.textContent.includes("Show more") ? "Show Less..." : "Show More...";
+}); */
+
+
+/* first review show more text
+function showMoreFunction() {
+  const dots = document.getElementById("dots");
+  const moreText = document.getElementById("more");
+  const btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Show more..."; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Show less..."; 
+    moreText.style.display = "inline";
+  }
+} */
+
+const noOfCharacter = 120;
+const contents = document.querySelectorAll(".contents");
+//console.log(contents)
+contents.forEach((content) => {
+  if(content.textContent.length < noOfCharacter){
+    content.nextElementSibling.style.display = "none";
+  }
+  else{
+    const displayText = content.textContent.slice(0, noOfCharacter);
+    // console.log(displayText);
+    const moreText = content.textContent.slice(noOfCharacter);
+    // console.log(moreText);
+    content.innerHTML = `${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>`;
+  }
 });
 
-const parentContainer1 = document.querySelector(".read-more-container1");
-
-parentContainer1.addEventListener("click", (event) => {
-  const current = event.target;
-  const isShowMoreBtn = current.className.includes("show-more-btn1");
-  if (!isShowMoreBtn) return;
-  const currentText = event.target.parentNode.querySelector(".read-more-text1");
-  currentText.classList.toggle("read-more-text--show1");
-  current.textContent = current.textContent.includes("Show more") ? "Show Less..." : "Show More...";
-});
-
-const parentContainer2 = document.querySelector(".read-more-container2");
-
-parentContainer2.addEventListener("click", (event) => {
-  const current = event.target;
-  const isShowMoreBtn = current.className.includes("show-more-btn2");
-  if (!isShowMoreBtn) return;
-  const currentText = event.target.parentNode.querySelector(".read-more-text2");
-  currentText.classList.toggle("read-more-text--show2");
-  current.textContent = current.textContent.includes("Show more") ? "Show Less..." : "Show More...";
-});
-
-const parentContainer3 = document.querySelector(".read-more-container3");
-
-parentContainer3.addEventListener("click", (event) => {
-  const current = event.target;
-  const isShowMoreBtn = current.className.includes("show-more-btn3");
-  if (!isShowMoreBtn) return;
-  const currentText = event.target.parentNode.querySelector(".read-more-text3");
-  currentText.classList.toggle("read-more-text--show3");
-  current.textContent = current.textContent.includes("Show more") ? "Show Less..." : "Show More...";
-});
+function showMore(btn) {
+  const post = btn.parentElement;
+  post.querySelector(".dots").classList.toggle("hide");
+  post.querySelector(".more").classList.toggle("hide");
+  btn.textContent == "Show more" ? btn.textContent = "Show less" : btn.textContent = "Show more";
+}
